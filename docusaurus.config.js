f25a1e98517d7d91a3bdb82ff499a8378d4e3cab
kor <kor@dwarshuis.com>
2024-06-15 09:58:26 +0200

    Comment out copyright notice

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,7 +125,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        // copyright: `Copyright © ${new Date().getFullYear()} Spec-Up-T.`,
       },
       prism: {
         theme: prismThemes.github,

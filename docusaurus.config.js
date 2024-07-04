c9d1614ccf797c9286630d2c9542648811af2061
kor <kor@dwarshuis.com>
2024-07-04 10:03:41 +0200

    Update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Spec-Up-T',
-  tagline: 'Specifications made simple',
+  tagline: 'A tool for writing specifications',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here

ddb0ea4eab60fe09be21503276d5e9e8fb205b97
kor <kor@dwarshuis.com>
2024-08-16 16:58:03 +0200

    Change title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Create rich specification documents for standards bodies and engineering projects">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

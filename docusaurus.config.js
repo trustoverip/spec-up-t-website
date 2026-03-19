c48b26a338c42023299c3e2a045cbda5c9141004
Kor Dwarshuis <proximus@fastmail.com>
2026-03-19 09:18:45 +0100

    fix: move onBrokenMarkdownLinks warning to markdown hooks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,6 @@ const config = {
   projectName: 'Spec-Up-T', // Usually your repo name.
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -35,6 +34,9 @@ const config = {
   },
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
   themes: ['@docusaurus/theme-mermaid'],
 
